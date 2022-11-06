225af37681151a2ebf42696685b68d59687918f0
TeaCondemns <tea.condemns@gmail.com>
2022-11-06 14:49:45 +0300

    Randomly player spawn in the world feature

--- Assets/Scripts/ChunkData.cs
+++ Assets/Scripts/ChunkData.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class ChunkData
 {
     public Vector2Int position;
-    public BlockType[,,] blocks;
+    public readonly BlockType[,,] blocks;
 
     public ChunkData(Vector2Int position, BlockType[,,] blocks) {
         this.position = position;
--- Assets/Scripts/GameWorld.cs
+++ Assets/Scripts/GameWorld.cs
@@ -3,16 +3,20 @@ using UnityEngine;
 
 public class GameWorld : MonoBehaviour
 {
+    public const int ChunksByX = 30;
+    public const int ChunksByY = 30;
+    
     public readonly Dictionary<Vector2Int, ChunkData> chunksData = new();
     public ChunkRenderer chunkPrefab;
+    public PlayerControls playerPrefab;
 
     void Start()
     {
-        for (var x = 0; x < 30; x++)
-            for (var y = 0; y < 30; y++) {
+        for (var x = 0; x < ChunksByX; x++)
+            for (var y = 0; y < ChunksByY; y++) {
                 const float ratio = ChunkRenderer.ChunkWidth * ChunkRenderer.BlockScale;
-                float xPos = x * ratio,
-                    zPos = y * ratio;
+                var xPos = x * ratio;
+                var zPos = y * ratio;
 
                 var chunkData = new ChunkData(
                     new Vector2Int(x, y),
@@ -26,5 +30,34 @@ public class GameWorld : MonoBehaviour
                 chunk.chunkData = chunkData;
                 chunk.parentWorld = this;
             }
+        
+        SpawnPlayer();
+    }
+
+    private void SpawnPlayer()
+    {
+        var chunkPos = new Vector2Int(Random.Range(0, ChunksByX - 1), Random.Range(0, ChunksByY - 1));
+
+        if (!chunksData.TryGetValue(chunkPos, out var chunkData)) return;
+        
+        var playerInChunkPos = new Vector2Int();
+
+        for (var i = 0; i < 2; i++)
+            playerInChunkPos[i] = Random.Range(0, ChunkRenderer.ChunkWidth - 1);
+
+        var playerWorldPos = new Vector3(
+            (playerInChunkPos.x + ChunkRenderer.ChunkWidth * chunkPos.x) * ChunkRenderer.BlockScale,
+            0,
+            (playerInChunkPos.y + ChunkRenderer.ChunkWidth * chunkPos.y) * ChunkRenderer.BlockScale
+            );
+
+        for (var chunkY = 0; chunkY < ChunkRenderer.ChunkHeight; chunkY++)
+            if (chunkData.blocks[playerInChunkPos.x, chunkY, playerInChunkPos.y] == BlockType.Air)
+            {
+                playerWorldPos.y = chunkY * ChunkRenderer.BlockScale;
+                break;
+            }
+        
+        Instantiate(playerPrefab, playerWorldPos, Quaternion.identity, transform);
     }
 }
